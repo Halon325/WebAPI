cd481de9f48201a3aee4a121d05dcac617372cdc
bibab <bibab@DESKTOP-1FH1TD9>
2023-04-13 14:51:14 +0300

    Добавьте файлы проекта.

---
+++ WebAPI/Models/ResponseHotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPI.Entity;
+
+namespace WebAPI.Models
+{
+    public class ResponseHotel
+    {
+        public ResponseHotel(Hotel hotel)
+        {
+            ID = hotel.Id;
+            Name = hotel.Name;
+            CountOfStars = (int)hotel.CountOfStars;
+            CountryCode = hotel.CountryCode ;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int CountOfStars { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
